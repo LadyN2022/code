cc3c03cd3dea00cb4b038238e1a7e9eba63db955
Vismura <natali-ivleva@list.ru>
2022-12-01 20:27:20 +0700

    HW1

---
+++ seminar1/HW1/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число ");
+        int num1 = Convert.ToInt32 (Console.ReadLine());
+        Console.WriteLine("Введите второе число ");
+        int num2 = Convert.ToInt32 (Console.ReadLine());  
+
+        if (num1 > num2)
+        {
+            Console.WriteLine(num1);
+        }
+        else if (num1 < num2)
+        {
+            Console.WriteLine(num2);
+        }
+        else
+        {
+            Console.WriteLine("Оба числа равны");
+        }
+    }
+}
